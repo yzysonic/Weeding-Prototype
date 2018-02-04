f7ba1e5581e55fa4729b5e09d6f1cd6bae1c2d81
YangTzui <yzysonic@gmail.com>
2018-02-04 17:56:52 +0900

    ゲームシーンの追加、プレイヤーの基本制御（移動）、カメラの追従制御（位置）

---
+++ Assets/Script/FollowPlayer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour {
+
+    public float speed = 5.0f;
+
+    private Transform target;
+    private Vector3 offset;
+    private float theta;
+    private float phi;
+    private float distance;
+
+	// Use this for initialization
+	void Start () {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+        offset = (transform.position - target.position);
+        theta = Mathf.Atan2(offset.y, (new Vector2(offset.x, offset.z)).magnitude);
+        phi = Mathf.Atan2(offset.x, offset.z);
+        distance = offset.magnitude;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = Vector3.Lerp(transform.position, target.position + offset, Time.deltaTime * speed);
+
+    }
+}
---
+++ Assets/Script/PlayerMove.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+
+    public float speed = 10;
+    public float rotate_speed = 10;
+
+    private Quaternion target_quat;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(x, 0.0f, z);
+        movement.Normalize();
+
+        if (movement.sqrMagnitude > 0.0f)
+            target_quat = Quaternion.LookRotation(movement);
+
+        var controller = GetComponent<CharacterController>();
+        controller.SimpleMove(movement * speed);
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, target_quat, Time.deltaTime * rotate_speed);
+
+    }
+
+}
